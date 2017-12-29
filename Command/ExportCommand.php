349bf4593a71be98017b2f0c4d32b1a677b518a7
Joris Ros <joris.ros@gmail.com>
2017-12-29 01:28:10 +0100

    Add the basic to run export from commandline

---
+++ Command/ExportCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FeedBuilderBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('feedbuilder:run')
+            ->setDescription('Run the feedbuilder by ID')
+            ->addArgument('feed_id', InputArgument::OPTIONAL, 'Argument description')
+            //->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $argument = $input->getArgument('argument');
+
+        if ($input->getOption('option')) {
+            // ...
+        }
+
+        $output->writeln('Command result.');
+    }
+
+}
