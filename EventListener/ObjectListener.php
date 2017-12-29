f3941c116540d6a7c4aaca1397d06651d326e74d
Joris Ros <joris.ros@gmail.com>
2017-12-29 01:17:29 +0100

    Add Event Listener for object

---
+++ EventListener/ObjectListener.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jorisros
+ * Date: 29/12/2017
+ * Time: 01:11
+ */
+
+namespace FeedBuilderBundle\EventListener;
+
+
+use Pimcore\Event\Model\ElementEventInterface;
+
+class ObjectListener
+{
+    public function doExport(ElementEventInterface $event){
+
+    }
+}
