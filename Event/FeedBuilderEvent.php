67ed05e019fef49dc750fb8bac1d78578db06947
Joris Ros <joris.ros@gmail.com>
2018-01-02 01:48:18 +0100

    Update the event class created the getters and setters

--- Event/FeedBuilderEvent.php
+++ Event/FeedBuilderEvent.php
@@ -9,6 +9,8 @@
 namespace FeedBuilderBundle\Event;
 
 
+use Pimcore\Config\Config;
+use Pimcore\Model\DataObject\Listing\Concrete;
 use Symfony\Component\EventDispatcher\Event;
 
 class FeedBuilderEvent extends Event
@@ -18,4 +20,49 @@ class FeedBuilderEvent extends Event
     const BEFORE_ROW = 'feedbuilder.before.row';
     const AFTER_ROW = 'feedbuilder.after.row';
     const AFTER_RUN = 'feedbuilder.after.run';
+
+    /** @var Config */
+    private $config = null;
+
+    /** @var Concrete */
+    private $listing;
+
+    /** @var \Pimcore\Model\DataObject\Concrete */
+    private $object;
+
+    private $array;
+
+    private $result;
+
+    public function setConfig(Config $config) {
+        $this->config = $config;
+    }
+    public function getConfig() {
+        return $this->config;
+    }
+    public function setListing(Concrete $listing) {
+        $this->listing = $listing;
+    }
+    public function getListing() {
+        return $this->listing;
+    }
+    public function setObject(\Pimcore\Model\DataObject\Concrete $object) {
+        $this->object = $object;
+    }
+    public function getObject() {
+        return $this->object;
+    }
+    public function setArray($array) {
+        $this->array = $array;
+    }
+    public function getArray() {
+        return $this->array;
+    }
+    public function setResult($result) {
+        $this->result = $result;
+    }
+    public function getResult() {
+        return $this->result;
+    }
+
 }
