8e1970705c174ae038a6dd40a58f5c5e93f63d07
Joris Ros <joris.ros@gmail.com>
2018-01-02 01:13:35 +0100

    Update the service container, adding the method to receive the config profile. And basic run function

--- Service/FeedBuilderService.php
+++ Service/FeedBuilderService.php
@@ -9,6 +9,8 @@
 namespace FeedBuilderBundle\Service;
 
 use Pimcore\Config;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\Object;
 
 class FeedBuilderService
 {
@@ -31,7 +33,41 @@ class FeedBuilderService
         return new Config\Config(include($systemConfigFile));
     }
 
-    private function getSelection(){
+    /**
+     * Returns the profile by ID or name
+     *
+     * @param $id
+     * @throws \Exception
+     */
+    public static function getConfigOfProfile($id) {
+        $config = self::getConfig();
 
+        if(is_integer($id)){
+            return $config->get('feeds')[$id];
+        }
+
+        if(is_string($id)){
+            foreach ($config->get('feeds') as $feed) {
+                if($feed->get('channel') === $id) {
+                    return $feed;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Run the feedbuilder
+     * 
+     * @param Config\Config $config
+     */
+    public function run(Config\Config $config) {
+        $class = $config->get('class');
+        $listing = $class.'\Listing';
+
+        $criteria = new $listing();
+        $criteria->setUnpublished(!$config->get('published'));
+        $objects = $criteria->load();
     }
 }
